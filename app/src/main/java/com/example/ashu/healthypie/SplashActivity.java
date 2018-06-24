7fb94d93674e52a5b5078479dac88e62bec3ddf7
207ashu <207ashu@gmail.com>
2018-06-24 12:50:41 +0530

    Splash Activity

---
+++ app/src/main/java/com/example/ashu/healthypie/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.ashu.healthypie;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this,ManageDevicesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
