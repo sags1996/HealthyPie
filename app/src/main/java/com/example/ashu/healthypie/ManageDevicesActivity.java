a2e390f0ef53bde12ccbb6484fc13fd1e55702cf
sags1996 <sagarika.sachdeva@gmail.com>
2018-06-30 22:55:43 +0530

    dependencies added

--- app/src/main/java/com/example/ashu/healthypie/ManageDevicesActivity.java
+++ app/src/main/java/com/example/ashu/healthypie/ManageDevicesActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class ManageDevicesActivity extends AppCompatActivity {
@@ -24,4 +26,6 @@ public class ManageDevicesActivity extends AppCompatActivity {
             }
         });
     }
+     
+
 }
